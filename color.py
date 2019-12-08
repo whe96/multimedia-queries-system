772175d8da0050de9dc5668e944fc873b9558f41
jiarongqiu <jiarong_qiu>
2019-12-08 11:22:52 -0800

    'color:write color histogram module and validate'

--- color.py
+++ color.py
@@ -1,32 +1,97 @@
 import cv2
 import numpy as np
+import os
+import glob
 from scipy import spatial
-from utils import read_image
+from utils import read_image, timeit
+from test import simulate_similarity
+
+class Color:
+
+    def __init__(self, dir, feature_dir, width, height, color_space='RGB', bins=32):
+        self.dir = dir
+        self.feature_dir = feature_dir
+        self.color_space = color_space
+        self.bins = bins
+        self.width = width
+        self.height = height
+        self.features = {}
+
+        if color_space == 'RGB':
+            self.weights = [1,1,1]
+        elif color_space == 'YCrCb':
+            self.weights = [2,1,1]
+
+    """
+        Output is 3 x bins
+    """
+    # @timeit
+    def cal_color_histogram(self,img_path):
+        img = read_image(img_path, self.width, self.height)
+        channel1 = cv2.calcHist([img], [0], None, [self.bins], [0, 256]).flatten()
+        channel2 = cv2.calcHist([img], [1], None, [self.bins], [0, 256]).flatten()
+        channel3 = cv2.calcHist([img], [2], None, [self.bins], [0, 256]).flatten()
+        hist = np.vstack([channel1, channel2, channel3])
+        return hist
 
 
-def cal_color_histogram(img):
-    channelR = cv2.calcHist([img],[0],None,[256],[0,256])
-    channelG = cv2.calcHist([img], [0], None, [256], [0, 256])
-    channelB = cv2.calcHist([img], [0], None, [256], [0, 256])
-    hist = np.hstack([channelR,channelG,channelB]).flatten()
-    return hist
+    def load(self):
+        features = glob.glob(os.path.join(self.feature_dir, '*'))
+        for feature in features:
+            name = os.path.basename(feature).split('.')[0]
+            npy = np.load(feature)
+            self.features[name] = npy
+        print("Finish loading color features from {}".format(features))
+
+    def preprocess(self):
+        videos = glob.glob(os.path.join(self.dir, '*'))
+        for video in videos:
+            name = os.path.basename(video)
+            output_filename = os.path.join(self.feature_dir,name)
+            res = []
+            for i in range(1,600+1):
+                img_path = self.get_img_path(name,i)
+                hist = self.cal_color_histogram(img_path)
+                res.append(hist)
+            res = np.array(res)
+            print(name,res.shape)
+            np.save(output_filename,res)
+
+    def validate(self, num_ites = 10):
+        simulate_similarity(self.compare,num_ites)
+
+
+    def get_img_path(self,name,idx):
+        return os.path.join(self.dir, name, "{}{:03d}.rgb".format(name, idx))
+
+    def second_to_frame(self,second, start_idx=1, max_idx=600, fps=30):
+        frame_idx = int(second * fps) + start_idx
+        if frame_idx > max_idx:
+            frame_idx -= 1
+        return frame_idx
+
+    # @timeit
+    def get_avg_histogram(self,video,start,query_duration=5):
+        start_idx = self.second_to_frame(start)
+        end_idx = self.second_to_frame(start+query_duration)
+        hists = self.features[video][start_idx:end_idx]
+        hists = np.average(hists,axis=0)
+        return hists
+
+    def compare(self,video1,start1,video2,start2):
+        feature1 = self.get_avg_histogram(video1,start1)
+        feature2 = self.get_avg_histogram(video2,start2)
+        res = []
+        for i in range(feature1.shape[0]):
+            res.append(1 - spatial.distance.cosine(feature1[i], feature2[i]))
+        similarity = sum([x*y/sum(self.weights) for x,y in zip(res,self.weights)])
+        return similarity
 
 if __name__ == '__main__':
-    paths = [
-        "/Users/jiarongqiu/Desktop/CS576/Final/database_videos/flowers/flowers001.rgb",
-        "/Users/jiarongqiu/Desktop/CS576/Final/database_videos/interview/interview001.rgb",
-        "/Users/jiarongqiu/Desktop/CS576/Final/database_videos/movie/movie001.rgb",
-        "/Users/jiarongqiu/Desktop/CS576/Final/database_videos/musicvideo/musicvideo001.rgb",
-        "/Users/jiarongqiu/Desktop/CS576/Final/database_videos/sports/sports001.rgb",
-        "/Users/jiarongqiu/Desktop/CS576/Final/database_videos/starcraft/starcraft001.rgb",
-        "/Users/jiarongqiu/Desktop/CS576/Final/database_videos/traffic/traffic001.rgb"
-    ]
-    features = []
-    for path in paths:
-        img = read_image(path,352,288)
-        feature = cal_color_histogram(img)
-        features.append(feature)
-    for i in range(len(features)):
-        for j in range(i,len(features)):
-            similarity = 1 - spatial.distance.cosine(features[j], features[i])
-            print(i,j,similarity)
+    dir = "data/dataset"
+    outdir = "feature/color"
+    color = Color(dir,outdir,352,288)
+    # color.preprocess()
+    color.load()
+    for i in [10,50,100,500,1000,2000,5000,10000]:
+        color.validate(i)
---
+++ test.py
@@ -0,0 +1,34 @@
+import random
+
+filenames = ["flowers", "interview", "movie", "musicvideo", "sports", "starcraft", "traffic"]
+max_duration = 20
+query_duration = 5
+
+
+def random_start_time():
+    start = random.uniform(0, max_duration-query_duration)
+    return start
+
+
+def simulate_similarity(func, num_iters=10):
+    distance = 0.0
+    for _ in range(num_iters):
+        video1 = random.sample(filenames, 1)[0]
+        start1 = random_start_time()
+
+        self_similarity = 0.0
+        for _ in range(len(filenames) - 1):
+            start2 = random_start_time()
+            self_similarity += func(video1, start1, video1, start2)
+        self_similarity /= len(filenames) - 1
+
+        other_simlarity = 0.0
+        for video2 in filenames:
+            if video2 == video1: continue
+            start2 = random_start_time()
+            other_simlarity += func(video1, start1, video2, start2)
+        other_simlarity /= len(filenames) - 1
+
+        distance += self_similarity - other_simlarity
+    distance /= num_iters
+    print("The average similarity diff over {} iterations is {:.3f}% ".format(distance*100.0,num_iters))
